1d89f551fa1862265e8161915dba71b740dc4fc5
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-04-19 23:40:46 -0700

    debugged votes endpoints

--- data/create-tables.js
+++ data/create-tables.js
@@ -3,7 +3,6 @@ const pool = require('../lib/utils/pool');
 run();
 
 async function run() {
-    console.log('HERE!')
     try {
         // await pool.connect();
         console.log(process.env.DATABASE_URL)
